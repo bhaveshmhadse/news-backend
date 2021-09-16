c459c5058be06f1c0cd7720f996860dabdca9c4b
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2021-09-16 15:50:54 +0530

    Deploy

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const axios = require("axios");
 
-const app = express();
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Had get request on /");
 });
+
 app.post("/", async (req, res) => {
   let currentDate = new Date().toLocaleDateString();
 
@@ -26,8 +28,12 @@ app.post("/", async (req, res) => {
   res.send(arrayOfArticles);
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Listening on 5000");
+app.get("/:id", (req, res) => {
+  res.send(`No Such Endpoint!!, /${req.params.id}`);
 });
+
+//added second line
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT}`);
+});
+// added one line
